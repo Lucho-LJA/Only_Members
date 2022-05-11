9a83fca0fdfab104692791e7d55f2009e26bb4be
Lucho-lja <ljallauca.a@gmail.com>
2022-05-11 17:08:20 -0500

    Add registration_controller and name, username columns

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+
+    private
+
+    def after_sign_up_path_for(resource)
+        root_path # or any other path
+    end
+
+    def sign_up_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
+    def acount_update_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+    end
+end
